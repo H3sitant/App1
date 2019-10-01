ee0073e4ff9ce755ce1c006f0e73a8252d597622
Isack <isackbreton@live.ca>
2019-10-01 00:00:31 -0400

    Validation

--- Recherche_char/rechercheChar.c
+++ Recherche_char/rechercheChar.c
@@ -1,25 +1,37 @@
 #include <stdio.h>
-
+ void recherche_position (char *mot,char lettre);
 int main(void)
 {
-	char mot[20];
-	char lettre;
-	char lettre_tampon;
-	printf("Quel est le mot dans lequel la lettre est recherchee\n");
-	gets( mot );
-	printf("\nle mot est : %s", mot);
-	
-	printf("\nQuel est la lettre rechercher dans le mot\n");
-	scanf(" %c", &lettre);
-	printf("\nLa lettre est : %c", lettre);
-	
-	for (int i=0;i<=19;i++)
-	{
-		lettre_tampon = mot[i];
-		if (lettre_tampon==lettre)
-		{
-			printf("\nLa lettre se trouve a la position %d",i+1);
-		}
-	}
+	//char mot[];
+	//char lettre;
+	//printf("Quel est le mot dans lequel la lettre est recherchee\n");
+	//gets( mot );
+	//printf("\nQuel est la lettre rechercher dans le mot\n");
+	//scanf(" %c", &lettre);
+	recherche_position("anticonstitutionnellement",'i');
+	recherche_position("bonjour",'e');
+	recherche_position("bonjour",'r');
+	recherche_position("allocommentcava",'a');
 	return 0;
+}
+
+void recherche_position (char *mot,char lettre)
+{
+	char lettre_tampon;
+	int i=0;
+	printf("\nle mot est : %s", mot);
+	printf("\nLa lettre est : %c", lettre);
+	do
+	{
+		lettre_tampon= mot[i];
+		i++;
+	}
+	while(lettre_tampon != '\0' && lettre_tampon != lettre);
+	if(lettre_tampon == '\0')
+	{
+		printf("\nLa lettre n'est pas dans le mots : -1");
+	}else 
+	{
+		printf("\nLa lettre est dans le mots a la position : %d",i-1);
+	}
 }
